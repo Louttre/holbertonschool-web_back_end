18fc8da31a8ada00af94bc034c72e797d141724e
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-07-16 13:22:39 +0200

    Update 4-rest-parameter.js

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,5 +1,7 @@
-export default function returnHowManyArguments() {
+export default function returnHowManyArguments(...args) {
   let num = 0;
-  for (let arg of args) num += 1;
+  for (let arg of args) {
+    num += 1;
+  }
   return num;
 }
