1d1ae6848347da60298398d7c41f53b303972874
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-07-16 15:16:55 +0200

    Update 10-loops.js

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,3 +1,4 @@
+// export default (array, appendString) => array.map((value) => appendString + value);
 export default function appendToEachArrayValue(array, appendString) {
   let idx = 0;
   for (let elem of array) {
