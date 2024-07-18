9fc4e9699f0ba8e2df2fca2e2a1730d33f0d8d09
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-07-18 13:40:52 +0200

    Update 1-make_classrooms.js

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,10 +1,11 @@
-/*eslint-disable*/
-import ClassRoom from `./0-classroom.js`
+import ClassRoom from './0-classroom.js';
 
 function initializeRooms() {
   return [
-     new ClassRoom(19),
-     new ClassRoom(20),
-     new ClassRoom(34)
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34)
   ];
 }
+
+export default initializeRooms;
