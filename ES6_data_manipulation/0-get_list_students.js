84fae5f6a1384bff192872c0bb41a647cf19613d
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-07-22 15:16:58 +0200

    Update 0-get_list_students.js

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,8 @@
 /*eslint-disable*/
 export default function getListStudents() {
-  const array = [{
-    id: 1,
-    firstname: 'Guillaume',
-    location: 'San Francisco'
-  }, {
-    id: 2,
-    firstname: 'James',
-    location: 'Columbia'
-  }, {
-    id: 5,
-    firstname: 'Serena',
-    location: 'San Francisco'
-  }]
-
-  let new_array = array.map(person => ({ id: person.id, firstName: person.firstname, location: person.location }));
-  return new_array;
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
 }
