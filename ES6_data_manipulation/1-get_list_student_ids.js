e7df57b60750722dc46bd20f02a65faa060a9d30
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-07-22 14:53:27 +0200

    Update 1-get_list_student_ids.js

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,8 @@
 /*eslint-disable*/
 function getListStudentIds(array) {
+  if (!Array.isArray(array)) {
+    return [];
+  }
   let new_array = array.map(person => person.id);
   return new_array;
 }
