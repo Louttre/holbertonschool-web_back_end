f844d4408bc3f2a6f4c4091e8165dea6e3f3480f
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-07-22 16:12:01 +0200

    Update 4-update_grade_by_city.js

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,5 @@
 /*eslint-disable*/
-export default function getStudentsByLocation(array, location) {
+function getStudentsByLocation(array, location) {
   let new_array = array.filter(person => person.location === location)
   return new_array;
 }
