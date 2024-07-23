4a7224a6b6866e8fedaaf85314cced307335f12e
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-07-23 11:41:08 +0200

    Update 4-update_grade_by_city.js

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,7 @@
 /*eslint-disable*/
-export default function updateStudentGradeByCity(array_student, location, array_grade) {
+export default function updateStudentGradeByCity(array_student, city, array_grade) {
   const gradeObj = newGrades.find(grade => grade.studentId === student.id);
-  let loc_array = array.filter(person => person.location === location)
+  let loc_array = array.filter(person => person.location === city)
   let final_array = loc_array.map(student => {
         return ({
           ...student,
