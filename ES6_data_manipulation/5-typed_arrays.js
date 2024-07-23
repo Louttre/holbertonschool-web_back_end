131fb08550a59d82ab84a9b294ca36c9e97cdb37
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-07-23 11:44:23 +0200

    Update 5-typed_arrays.js

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,5 @@
 /*eslint-disable*/
-function createInt8TypedArray(length, position, value) {
+export default function createInt8TypedArray(length, position, value) {
     const buffer = new ArrayBuffer(length);
     const view = new DataView(buffer);
     if (position < 0 || position >= length) {
