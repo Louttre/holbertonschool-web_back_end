6a54ae2b11d45fef499e041bd7db6bc4a910f876
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-07-23 11:59:43 +0200

    Create 8-clean_set.js

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+/*eslint-disable*/
+export default function cleanSet(set, startString) {
+    if (!startString) {
+        return '';
+    }
+    const result = [];
+
+    for (let value of set) {
+        if (value.startsWith(startString)) {
+            result.push(value.slice(startString.length));
+        }
+    }
+    return result.join('-');
+}
