1d78edc4a5d1017bb9f7ec1febbbdac49e702f51
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-07-24 13:52:55 +0200

    Create 3-all.js

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+/*eslint-disable*/
+import { uploadPhoto, createUser} from './utils.js';
+
+export default function handleProfileSignup() {
+  promise.all([
+    uploadPhoto(), 
+    createUSer()
+  ]).then((array) => {
+    [photo, user] = array;
+    console.log('${photo.body} ${user.firstName} ${user.lastName}');
+  }).catch(() => {
+    console.log('Signup system offline');
+  });
+}
