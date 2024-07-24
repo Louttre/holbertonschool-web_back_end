1d0b18e08098871b95a015716b318fbde2745851
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-07-24 14:17:51 +0200

    Update 5-photo-reject.js

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,6 @@
 /*eslint-disable*/
 export default function uploadPhoto(fileName) {
   return new Promise((reject) => {
-    reject({new Error(`${fileName} cannot be processed`});
+    reject(new Error(`${fileName} cannot be processed`));
+  });
 }
