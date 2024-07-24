6e46403a8feaecb8e525102033f3f97c945143c6
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-07-24 16:04:08 +0200

    Update 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -7,11 +7,7 @@ function handleProfileSignup(firstName, lastName, fileName) {
     signUpUser(firstName, lastName),
     uploadPhoto(fileName)
   ]).then((results) => {
-    return results.map((result) => {
-      return {
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason
-      };
-    });
-  });
-}
+    return results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.result : result.reason,
+    })));
