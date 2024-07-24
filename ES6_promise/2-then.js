144ef2772400627d220744eeb014bf97a92018b9
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-07-24 13:07:19 +0200

    Update 2-then.js

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -10,5 +10,5 @@ export default function handleResponseFromAPI(promise) {
     })
     .catch(() => {
       console.log('Got a response from the API');
-      new Error()});
+      return new Error()});
   }
