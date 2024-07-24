340606d1718cef98d170e96d5ccf904250dee381
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-07-24 13:09:49 +0200

    Update 2-then.js

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,12 +1,14 @@
-/* eslint-disable */
+/*eslint-disable*/
 export default function handleResponseFromAPI(promise) {
-    return promise
-      .then(() => {
-        console.log('Got a response from the API');
-        return {
-          status: 200,
-          body: 'success',
-        };
-      })
-      .catch(() => new Error());
+  return promise
+  .then(() => {
+    console.log('Got a response from the API');
+    return {
+      status: 200,
+      body: 'Success'
+    };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error()});
   }
