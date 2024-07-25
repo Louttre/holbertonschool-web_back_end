bef2c8e6776076e2f498cf7d80131b48e838a387
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-07-25 09:37:27 +0200

    Update 10-update_uniq_items.js

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,14 +1,14 @@
-/*eslint-disable*/
-export default function updateUniqueItems(map) {
-    if (!(map instanceof Map)) {
+/* eslint-disable */
+export default function updateUniqueItems(groceries) {
+    if (!(groceries instanceof Map)) {
         throw new Error('Cannot process');
     }
 
-    for (let [key, value] of map) {
-        if (value === 1) {
-            map.set(key, 100);
+    for (const [item, quantity] of groceries.entries()) {
+        if (quantity === 1) {
+            groceries.set(item, 100)
         }
     }
 
-    return map;
+    return groceries;
 }
