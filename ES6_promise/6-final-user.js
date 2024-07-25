2a48f2d9afa8ccc556240e2f86f1567c04fe1879
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-07-25 09:23:33 +0200

    Update 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -11,4 +11,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
       return {
         status: result.status,
         value: result.status === 'fulfilled' ? result.value : result.reason.message
-      }})})};
+      }
+    })
+  })
+};
