5acc1460612c4786109a192179e730419ce4dc42
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-08-18 15:19:43 +0200

    Update 4-define_variables.py

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 
 a: int = 1
+"""
+a (int): An integer variable representing the value 1.
+"""
+
 pi: float = 3.14
+"""
+pi (float): A floating-point variable representing the mathematical constant π, 
+approximated to 3.14.
+"""
+
 i_understand_annotations: bool = True
+"""
+i_understand_annotations (bool): A boolean variable indicating whether 
+annotations are understood, set to True.
+"""
+
 school: str = "Holberton"
+"""
+school (str): A string variable representing the name "Holberton".
+"""
