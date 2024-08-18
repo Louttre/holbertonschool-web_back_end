6c21eddfb856a35eaa7c49afd85ff45581d24eba
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-08-18 15:21:01 +0200

    Update 5-sum_list.py

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,4 +1,18 @@
+#!/usr/bin/env python3
+"""module"""
+
+
 def sum_list(input_list: list[float]) -> float:
+    """
+    Sums all the floating-point numbers in a list and returns the total.
+
+    Parameters:
+    input_list (list[float]): A list of floating-point numbers to be summed.
+
+    Returns:
+    float: The sum of all numbers in the input list.
+    """
+
     i = 0
     for num in input_list:
         i += num
