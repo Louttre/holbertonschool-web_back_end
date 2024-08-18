47b4afe6bcfc1a76081ccdfcae23a3dd28eef650
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-08-18 15:21:32 +0200

    Update 6-sum_mixed_list.py

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
+"""module"""
+
 
 def sum_mixed_list(mxd_lst: list[float | int]) -> float:
+    """
+    Sums all the numbers in a list containing both integers and floating-point numbers, 
+    and returns the total as a float.
+
+    Parameters:
+    mxd_lst (list[float | int]): A list of numbers, which can include both 
+    integers and floating-point values.
+
+    Returns:
+    float: The sum of all numbers in the input list, returned as a float.
+    """
     i = 0
     for num in mxd_lst:
         i += num
