7747477fad509e4a9709d84fb9d219e876d18426
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-08-18 15:29:48 +0200

    Update 5-sum_list.py

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -3,7 +3,7 @@
 from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: typing.List[float]) -> float:
     """
     Sums all the floating-point numbers in a list and returns the total.
 
