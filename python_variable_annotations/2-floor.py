7a71624e77b03e1753b0dc98379f763fdbbdbcd2
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-08-18 15:17:20 +0200

    Update 2-floor.py

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+"""module"""
+
 
 def floor(n: float) -> int:
-    return int(str1)
+    """
+    Returns the largest integer less than or equal to the given floating-point number.
+    
+    This function converts the given float to an integer by truncating the decimal part,
+    effectively rounding down towards zero.
+
+    Parameters:
+    n (float): The floating-point number to be floored.
+
+    Returns:
+    int: The largest integer less than or equal to the input number.
+    """
+    return int(n)
