468d7fda51e8ac1053beee5c5bbfe8063fbdfe14
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-08-18 15:17:55 +0200

    Update 3-to_str.py

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+"""module"""
+
 
 def to_str(n: float) -> str:
+    """
+    Converts a floating-point number to its string representation.
+
+    Parameters:
+    n (float): The floating-point number to be converted to a string.
+
+    Returns:
+    str: The string representation of the input number.
+    """
     return str(n)
