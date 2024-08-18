d9b73fe878672d5c9ccdf476972f6236381c088e
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-08-18 15:27:11 +0200

    Update 4-define_variables.py

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -9,13 +9,13 @@ a (int): An integer variable representing the value 1.
 
 pi: float = 3.14
 """
-pi (float): A floating-point variable representing the mathematical constant π, 
+pi (float): A floating-point variable representing the mathematical constant π,
 approximated to 3.14.
 """
 
 i_understand_annotations: bool = True
 """
-i_understand_annotations (bool): A boolean variable indicating whether 
+i_understand_annotations (bool): A boolean variable indicating whether
 annotations are understood, set to True.
 """
 
