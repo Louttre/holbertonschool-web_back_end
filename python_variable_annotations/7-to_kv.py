3e3f85f5f32ac447f64c3ceecede900ec947e9ab
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-08-18 15:22:06 +0200

    Update 7-to_kv.py

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
+"""module"""
+
 
 def to_kv(k: str, v: int | float) -> tuple[str, float]:
+    """    
+    Creates a tuple with a string and the square of a number.
+
+    Parameters:
+    k (str): The key, represented as a string.
+    v (int | float): A number that can be either an integer or a float.
+
+    Returns:
+    tuple[str, float]: A tuple where the first element is the string `k`, 
+    and the second element is the square of `v`, represented as a float.
+    """
     return (k, v * v)
