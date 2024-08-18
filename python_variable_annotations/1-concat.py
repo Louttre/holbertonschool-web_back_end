4c64232c68eba9656b19fe4d9103fa1ef14196f1
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-08-18 15:16:33 +0200

    Update 1-concat.py

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+"""module"""
+
 
 def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the resulting string.
+
+    Parameters:
+    str1 (str): The first string to be concatenated.
+    str2 (str): The second string to be concatenated.
+
+    Returns:
+    str: The concatenation of the two input strings.
+    """
     return str1 + str2
