f85a284f308901cb1e551489aad5de6932402ca9
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-08-19 16:48:10 +0200

    Update 2-hypermedia_pagination.py

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -41,18 +41,18 @@ class Server:
             return []
         return dataset[start_index:end_index]
 
-      def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Optional[int]]:
-          dataset = self.get_page(page, page_size)
-          start_index, end_index = index_range(page, page_size)
-          next_page = page + 1 if end_index < len(dataset) else None
-          prev_page = page - 1 if page > 1 else None
-          total_pages = math.ceil(len(dataset) / page_size)
-          return {
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Optional[int]]:
+        dataset = self.get_page(page, page_size)
+        start_index, end_index = index_range(page, page_size)
+        next_page = page + 1 if end_index < len(dataset) else None
+        prev_page = page - 1 if page > 1 else None
+        total_pages = math.ceil(len(dataset) / page_size)
+        return {
               'page_size': page_size,
               'page': page,
               'data': dataset,
               'next_page': next_page,
-              'prev_page': prev_page
+              'prev_page': prev_page,
               'total_pages': total_pages
-          }
+         }
               
