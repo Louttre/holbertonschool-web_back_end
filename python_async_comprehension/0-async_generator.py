a2c37c7f1e5ce0412001d8ff71f398539e869b90
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-08-19 14:13:59 +0200

    Update 0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """module"""
 import asyncio
-import time
 from random import uniform
+from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """
