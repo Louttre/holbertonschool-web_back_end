10cf7d9f9c46719397c8778490a7cf59c9c509bd
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-08-19 14:50:47 +0200

    Update 1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -9,7 +9,8 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def print_yielded_values() -> List[float]:
     """
-    Asynchronous function that collects values yielded by async_generator
+    Asynchronous function that collects values
+    yielded by async_generator
     into a list and then prints that list.
     """
     return result = [i async for i in async_generator()]
