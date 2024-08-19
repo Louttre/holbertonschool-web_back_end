253907d2a09a00ab51e40998bb80f0acd097b7b1
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-08-19 14:50:20 +0200

    Update 0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,10 +7,13 @@ from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """
-    An asynchronous generator that yields a random number between 0 and 10.
+    An asynchronous generator that yields
+    a random number between 0 and 10.
 
-    The generator runs a loop 10 times. In each iteration, it waits asynchronously for 1 second 
-    using asyncio.sleep(1), and then yields a random floating-point number between 0 and 10.
+    The generator runs a loop 10 times.
+    In each iteration, it waits asynchronously for 1 second
+    using asyncio.sleep(1), and then yields
+    a random floating-point number between 0 and 10.
     """
     for _ in range(10):
         await asyncio.sleep(1)
