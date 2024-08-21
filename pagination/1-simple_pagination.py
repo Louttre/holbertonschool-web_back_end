6a99cac6dad3dcff8b3bb4cb313e10d6a1de2627
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-08-21 12:26:00 +0200

    Update 1-simple_pagination.py

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-''' Simple pagination '''
+"""module"""
+from typing import Tuple
 import csv
 import math
-from typing import List, Tuple
+from typing import List
 
 
 class Server:
@@ -25,20 +26,19 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        ''' def get page '''
         assert type(page_size) is int and type(page) is int
         assert page > 0
         assert page_size > 0
-        self.dataset()
-        i = index_range(page, page_size)
-        if i[0] >= len(self.__dataset):
+        dataset = self.dataset()
+        start_index, end_index = index_range(page, page_size)
+        if start_index >= len(dataset):
             return []
-        else:
-            return self.__dataset[i[0]:i[1]]
+        return dataset[start_index:end_index]
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    ''' De*f index range '''
-    index = page * page_size - page_size
-    index_1 = index + page_size
-    return (index, index_1)
+    """
+    Calculate the start and end indices
+    for a given page and page size in pagination.
+    """
+    return ((page * page_size - page_size), page * page_size)
