771e901a4ec49f528685a2fca3889a24d1cc14c8
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-08-21 17:59:42 +0200

    Update 12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -19,6 +19,10 @@ method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 print(f'{mongo_collection.count_documents()} logs')
 print("Methods :")
 for met in method:
-    print(f'\tmethod {met}: {mongo_collection.count_documents({ "method": met })}')
-status_check_count = mongo_collection.count_documents({ "method": "GET", "path": "/status" })
+    print(f'\tmethod {met}: {mongo_collection.count_documents(
+        { "method": met }
+    )}')
+status_check_count = mongo_collection.count_documents(
+    { "method": "GET", "path": "/status" }
+)
 print(f'{status_check_count} status check')
