ed3e490d0c72eea217364435c403f5b875a2ac14
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-08-21 12:19:38 +0200

    Update 1-simple_pagination.py

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -34,8 +34,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert type(page) is int and page > 0, "Page should be a positive integer"
-        assert type(page_size) is int and page_size > 0, "Page_size should be a positive integer"
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
         dataset = self.dataset()
         start_index, end_index = index_range(page, page_size)
         if start_index >= len(dataset):
