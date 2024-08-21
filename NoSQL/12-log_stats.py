fbca08c1f13565bb5b050bb81eae60951224b346
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-08-21 18:03:09 +0200

    Update 12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -11,16 +11,16 @@ if __name__ == "__main__":
         Collection: nginx
     """
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client.logs
-mongo_collection = db.nginx
-method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-print(f'{mongo_collection.count_documents()} logs')
-print("Methods:")
-for met in method:
-    count = mongo_collection.count_documents({ "method": met })
-    print(f'\tmethod {met}: {count}')
-status_check_count = mongo_collection.count_documents(
-    { "method": "GET", "path": "/status" }
-)
-print(f'{status_check_count} status check')
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client.logs
+    mongo_collection = db.nginx
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print(f'{mongo_collection.count_documents()} logs')
+    print("Methods:")
+    for met in method:
+        count = mongo_collection.count_documents({ "method": met })
+        print(f'\tmethod {met}: {count}')
+    status_check_count = mongo_collection.count_documents(
+        { "method": "GET", "path": "/status" }
+    )
+    print(f'{status_check_count} status check')
