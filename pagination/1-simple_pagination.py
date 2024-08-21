2a5b4735a01eeb0dacca37649e8177fa2b059e4c
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-08-21 12:21:29 +0200

    Update 1-simple_pagination.py

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -11,7 +11,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     Calculate the start and end indices
     for a given page and page size in pagination.
     """
-    return ((page - 1) * page_size, page * page_size)
+    return ((page * page_size - page_size), page * page_size)
 
 
 class Server:
