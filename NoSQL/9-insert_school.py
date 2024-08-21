4dedc4a4160ba5c18dcf9ea3ec8980248516434a
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-08-21 17:36:16 +0200

    Update 9-insert_school.py

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -9,6 +9,9 @@ mongo_collection = db.school
 
 
 def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a document into the MongoDB collection and returns the inserted document's ID.
+    """
     document = kwargs
     inserted_doc = mongo_collection.insert_one(document)
     inserted_doc_id = inserted_doc.inserted_id
